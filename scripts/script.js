b9a2c82b0359513ed6e1c2105d36727cacc5354a
rvdegroen <68691966+rvdegroen@users.noreply.github.com>
2021-10-06 14:33:59 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,51 @@
 console.log("test");
+/*
+var hamburgerButton = document.querySelector("header button:first-of-type"),
+    closeNavButton = document.querySelector("header button:nth-of-type(2)"),
+    mobileNav = document.querySelector("nav"),
+    //de streepjes die ik wil animeren in het hamburger menu 
+    hambrugerStreep1 = document.querySelector("header > button span:nth-of-type(1)"),
+    hamburgerStreep2 = document.querySelector("header > button span:nth-of-type(2)"),
+    hamburgerStreep3 = document.querySelector("header > button span:nth-of-type(3)");
 
-var navBtnEl = document.querySelector("nav button"),
-    nav = document.querySelector("nav"),
-    hamburgerMenu = document.querySelector("header h1 img");
 
-function hideNav() {
-    nav.classList.add("hideNav");
+//mobileNav.classList.add("hidenav");
+mobileNav.classList.add("hidenav");
+
+function nav() {
+    hambrugerStreep1.classList.add("streep1");
+    hamburgerStreep2.classList.add("streep2");
+    hamburgerStreep3.classList.add("streep3");
 }
 
-function showNav() {
-    nav.classList.remove("hideNav");
+hamburgerButton.addEventListener("click", nav);
+*/
+
+var hamburgerButton = document.querySelector("header button:first-of-type"),
+    header = document.querySelector("header"),
+    closeNavButton = document.querySelector("nav > button");
+
+function navClose() {
+    header.classList.add("hidenav");
 }
 
-navBtnEl.addEventListener("click", hideNav);
-hamburgerMenu.addEventListener("click", showNav);
+function navToggle() {
+    header.classList.toggle("hidenav");
+}
+
+hamburgerButton.addEventListener("click", navToggle);
+closeNavButton.addEventListener("click", navClose);
+
+/*hamburger menu icon hover animation*/
+/*header > button span:nth-of-type(1){
+    background:transparent;
+    transform: translateX(-1.5em);
+}
+
+header > button span:nth-of-type(2){
+    transform: rotate(-45deg);
+}
+
+header > button span:nth-of-type(3){
+    transform: rotate(45deg);
+}*/
